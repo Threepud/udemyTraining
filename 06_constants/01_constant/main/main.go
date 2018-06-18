bc8d87afb3717770fbee949af05252dc8f3d1b00
Jacques Coetzee <>
2018-06-18 14:56:11 +0200

    Constants

---
+++ 06_constants/01_constant/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const p = "death and tax"
+
+func main() {
+
+	const q = "testing"
+
+	fmt.Println(p)
+	fmt.Println(q)
+}
---
+++ 06_constants/02_multiple-instantiation/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	Language = "Go"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Println(Language)
+}
